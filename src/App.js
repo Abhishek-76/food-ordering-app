68f7815e9a2a78a14587fea3987be9dffa1c5b67
Abhishek-76 <avinashgautam8923933406@gmail.com>
2022-11-18 13:26:51 +0530

    made

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import Cart from './components/Cart/Cart';
-import CartProvider from './store/CartProvider';
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -23,6 +23,10 @@ function App() {
       <main>
         <Meals />
       </main>
+      <div>
+        Made with{" "}
+        <a href="https://abhishek-joy-boy-coder.vercel.app/"> AbhishekGautam</a>
+      </div>
     </CartProvider>
   );
 }
